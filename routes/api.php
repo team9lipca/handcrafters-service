fc62a519d45711ab0d8500b6bac5a3d999c2345c
piatkiewicz <piatkiewicz.piotr@gmail.com>
2018-07-28 16:59:21 +0200

    api routki

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Craft;
+use App\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,6 +19,34 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('crafts', 'API\CraftController');
+
+Route::get('/crafts-main/{page}', function ($page) {
+    $craftsDto = Craft::skip(($page-1)*10)->take(10)->get();
+
+    foreach($craftsDto as $key => $craft) {
+        $craftsDto[$key]['author'] = User::findOrFail($craft['author_id']);
+    }
+
+    return ['crafts' => $craftsDto];
+});
+
+Route::get('/users-main/{page}', function ($page) {
+    $usersDto = User::skip(($page-1)*10)->take(10)->get();
+
+    foreach($usersDto as $key => $user) {
+        $usersDto[$key]['last-crafts'] = Craft::where(['author_id' => $user['id']])->orderBy('created_at')->take(5)->get();
+    }
+
+    return ['users' => $usersDto];
+});
+
+Route::get('/user-crafts-main/{username}/{page}', function ($username, $page) {
+    $user = User::where(['name' => $username])->first();
+
+    $craftsDto = Craft::where(['author_id' => $user['id']])->skip(($page-1)*10)->take(10)->get();
+
+    return ['crafts' => $craftsDto];
+});
 
 Route::get('dbtest', function() {
     DB::connection()->getPdo();
