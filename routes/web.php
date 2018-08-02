7c739254df0cba01ad8c741d62179d48418fc7c2
piatkiewicz <piatkiewicz.piotr@gmail.com>
2018-08-02 21:20:14 +0200

    ustawienie strony pod angulara

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
