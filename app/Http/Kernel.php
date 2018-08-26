cee815815f2f98e27fe72f687a196d8bc3d3082a
piatkiewicz <piatkiewicz.piotr@gmail.com>
2018-08-26 22:50:09 +0200

    options fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,11 +15,11 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \App\Http\Middleware\CorsMiddleware::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class
-        //\App\Http\Middleware\CorsMiddleware::class
     ];
 
     /**
--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -5,6 +5,10 @@ namespace App\Http\Middleware;
 class CorsMiddleware {
     public function handle($request, \Closure $next)
     {
+        if ($request->getMethod() === "OPTIONS") {
+            return response('');
+        }
+
         $response = $next($request);
         $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
         $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
